d2ca1d8a1347f1802b58929946ce50b941bb8bf4
Rosa Kang <therosessom@gmail.com>
2023-08-28 18:03:54 -0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Inspirational Quote Generator',
+  description: 'A fun app to generate quotes',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
